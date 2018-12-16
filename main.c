34f15f87283ae53fcfe31c8286d1d7fb9adf3eb8
Oliver Broad <oliverab@users.noreply.github.com>
2018-12-16 17:58:43 +0000

    LEDs flash on port B,
    Code runs on an "xpress" board, and improvised adapter connects xpress to an old EasyPIC4. Currently the xpress is powering the EP4.

--- main.c
+++ main.c
@@ -9,5 +9,13 @@
 #include <xc.h>
 
 void main(void) {
+    TRISB=0x00;
+    while (1)
+    {
+        PORTB=0x55;
+        __delay_ms(200);
+        PORTB=0xAA;
+        __delay_ms(200);
+    }
     return;
 }
